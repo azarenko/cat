7c7872c6a231e9473c3f2fc3a5a9a045cf8f62cd
sergey.azarenko <sergey.azarenko@valtech.com>
2024-05-24 15:37:42 +0300

    init

---
+++ cat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace cat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists(args[0]))
+            {
+                return;
+            }
+
+            using (StreamReader sr = new StreamReader(args[0]))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+        }
+    }
+}
